d1d6107a8e5f3cee2af95dfe15849d6b9ca74181
DebpriyaSinha <73025814+DebpriyaSinha@users.noreply.github.com>
2022-02-22 17:18:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -193,16 +193,16 @@ function spawnObstacles() {
   }
 }
 
-// function reset(){
-//   gameState = PLAY;
-//   score = 0;
-//   gameOver.visible = false;
-//   restart.visible = false;
-//   kangaroo.visible = true;
-//   kangaroo.changeAnimation("running", kangaroo_running);
-//   obstaclesGroup.destroyEach();
-//   shrubsGroup.destroyEach();
-// }
+ function reset(){
+   gameState = PLAY;
+   score = 0;
+   gameOver.visible = false;
+   restart.visible = false;
+   kangaroo.visible = true;
+   kangaroo.changeAnimation("running", kangaroo_running);
+   obstaclesGroup.destroyEach();
+   shrubsGroup.destroyEach();
+ }
 
 // function reset{
 //   gameState = PLAY;
@@ -215,16 +215,16 @@ function spawnObstacles() {
 //   shrubsGroup.destroyEach();
 // }
 
- function reset(){
-   gameState = PLAY;
-   score = 200;
-   gameOver.visible = false;
-   restart.visible = false;
-   kangaroo.visible = true;
-   kangaroo.changeAnimation("running", kangaroo_running);
-   obstaclesGroup.destroyEach();
-   shrubsGroup.destroyEach();
- }
+ //function reset(){
+  // gameState = PLAY;
+  //score = 200;
+  //gameOver.visible = false;
+     //restart.visible = false;
+     //kangaroo.visible = true;
+     //kangaroo.changeAnimation("running", kangaroo_running);
+     //obstaclesGroup.destroyEach();
+     //shrubsGroup.destroyEach();
+// }
 
 // function reset(){
 //   gameState === PLAY;
